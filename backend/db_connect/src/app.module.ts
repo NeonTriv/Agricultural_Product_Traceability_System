59a777b3a5a7e9b8e0480b524eb40869928bbea8
Nhan <nnhn2953@gmail.com>
2025-09-27 15:55:00 +0700

    changing for test

--- backend/db_connect/src/app.module.ts
+++ backend/db_connect/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'your-db-username',
+      password: 'your-db-password',
+      database: 'contact_form_db',
+      entities: [Student],  // entity
+      synchronize: true,
+      options: {
+        encrypt: true,
+        trustServerCertificate: true,
+      },
+    }),
+    ContactModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
